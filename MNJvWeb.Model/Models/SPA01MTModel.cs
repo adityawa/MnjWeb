d468d0b2299011f22e3871d9df45dea6ccfb8e38
adit <adityawa@outlook.com>
2018-08-11 08:14:24 +0700

    2

---
+++ MNJvWeb.Model/Models/SPA01MTModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNJvWeb.Model.Models
+{
+    public class SPA01MTModel
+    {
+        public string GRP_CD { get; set; }
+        public string ITEM_CD { get; set; }
+        public string ITEM_NM { get; set; }
+        public string USAGE { get; set; }
+    }
+}
---
+++ MNJvWeb/Models/EntityModel/SPA01MTModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MNJvWeb.Models.EntityModel
+{
+    public class SPA01MTModel
+    {
+        public string GRP_CD { get; set; }
+        public string ITEM_CD { get; set; }
+        public string ITEM_NM { get; set; }
+        public string USED { get; set; }
+        public string Class { get; set; }
+    }
+
+    
+}
